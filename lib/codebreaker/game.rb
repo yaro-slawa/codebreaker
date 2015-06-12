689492c7adcb4abb276e747e60d00ccd55a8a879
Yaroslawa Chernopischenko <chernopyshenko@gmail.com>
2015-06-12 15:02:56 +0300

    Add hint method

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -2,21 +2,24 @@ module Codebreaker
   class Game
   	CODE_LENGTH = 4
   	ATTEMPT_COUNT = 10
+  	HINT_COUNT = 1
 
-  	attr_reader :attempt
+  	attr_reader :attempts_made, :hints_made, :secret_code, :hint_str
 
   	def initialize
-  	  @attempt = 0
+  	  @attempts_made = 0
+  	  @hints_made = 0
   	end
 
   	def start
   	  @secret_code = CODE_LENGTH.times.map {Random.rand(1..6)}.join
+  	  @hint_str = "*" * CODE_LENGTH
   	end
 
   	def guess(code)
-  	  @attempt += 1	
+  	  @attempts_made += 1	
   	  marked_guess = ""
-  	  secret = @secret_code
+  	  secret = @secret_code.dup
 
       code.each_char.with_index do |c, i|
         if secret[i] == c
@@ -31,6 +34,12 @@ module Codebreaker
         end
       end
       marked_guess
+    end
+
+    def hint
+      @hint_str[@hints_made] = @secret_code[@hints_made]
+      @hints_made += 1
+      @hint_str
     end
   end
 end
