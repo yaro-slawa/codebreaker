c7f8ddeaba8562916e5d9311f45c3ed9befd9e3a
Yaroslawa Chernopischenko <chernopyshenko@gmail.com>
2015-06-12 15:39:29 +0300

    Modify hint method and spec on hint

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -1,46 +1,46 @@
 module Codebreaker
   class Game
-  	CODE_LENGTH = 4
-  	ATTEMPT_COUNT = 10
-  	HINT_COUNT = 1
+    CODE_LENGTH = 4
+    ATTEMPT_COUNT = 10
+    HINT_COUNT = 1
 
-  	attr_reader :attempts_made, :hints_made
+    attr_reader :attempts_made, :hints_made
 
-  	def initialize
-  	  @attempts_made = 0
-  	  @hints_made = 0
-  	end
+    def initialize
+      @attempts_made = 0
+      @hints_made = 0
+    end
 
-  	def start
-  	  @secret_code = CODE_LENGTH.times.map {Random.rand(1..6)}.join
-  	  @hint_str = "*" * CODE_LENGTH
-  	end
+    def start
+      @secret_code = CODE_LENGTH.times.map {Random.rand(1..6)}.join
+      @hint_str = "*" * CODE_LENGTH
+    end
 
-  	def guess(code)
-  	  @attempts_made += 1	
-  	  marked_guess = ""
-  	  secret = @secret_code.dup
+    def guess(code)
+      @attempts_made += 1	
+      marked_guess = ""
+      secret = @secret_code.dup
 
-  	  code.each_char.with_index do |c, i|
-  	  	if secret[i] == c
-  	  	  marked_guess += "+"
-  	  	  secret[i] = "*"
-  	  	  code[i] = "0"
-  	  	end
-  	  end
-  	  code.each_char.with_index do |c, i|
-  	    if (secret[i] != c && secret.include?(c))
-  	  	  marked_guess += "-"
-  	  	  secret[i] = "*"
-  	  	end
-  	  end
-  	  marked_guess
-  	end
+      code.each_char.with_index do |c, i|
+        if secret[i] == c
+          marked_guess += "+"
+          secret[i] = "*"
+          code[i] = "0"
+      end
+      end
+      code.each_char.with_index do |c, i|
+        if (secret[i] != c && secret.include?(c))
+          marked_guess += "-"
+          secret[i] = "*"
+      end
+      end
+      marked_guess
+    end
 
-  	def hint
-  	  @hint_str[@hints_made] = @secret_code[@hints_made]
-  	  @hints_made += 1
-  	  @hint_str
-  	end
+    def hint
+      @hint_str[@hints_made] = @secret_code[@hints_made]
+      @hints_made += 1
+      @hint_str
+    end
   end
 end
--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -2,50 +2,50 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do
-  	context "#start" do
-  	  let(:game) {Game.new}
+    context "#start" do
+      let(:game) {Game.new}
 
-  	  before {game.start}
+      before {game.start}
 
-  	  it "generates secret code" do
-  	  	expect(game.instance_variable_get(:@secret_code)).not_to be_empty 
-  	  end
-  	  it "saves 4 numbers secret code" do
-  	  	expect(game.instance_variable_get(:@secret_code).length).to eql(4)
-  	  end
-  	  it "saves secret code with numbers from 1 to 6" do
-  	    expect(game.instance_variable_get(:@secret_code)).to match(/[1-6]+/)
-  	  end
-  	end
+      it "generates secret code" do
+        expect(game.instance_variable_get(:@secret_code)).not_to be_empty 
+      end
+      it "saves 4 numbers secret code" do
+        expect(game.instance_variable_get(:@secret_code).length).to eql(4)
+      end
+      it "saves secret code with numbers from 1 to 6" do
+        expect(game.instance_variable_get(:@secret_code)).to match(/[1-6]+/)
+      end
+    end
 
-  	context "#guess" do
-  	  let(:game) {Game.new}
+    context "#guess" do
+      let(:game) {Game.new}
 
-  	  before {game.start}
+      before {game.start}
 
-  	  it "receive user's guess" do
-  	    allow(game.guess("1234")).to receive(:code)
-  	  end 
+      it "receive user's guess" do
+        allow(game.guess("1234")).to receive(:code)
+      end 
 
-  	  it "increments attempts that are made" do
-  	    old = game.instance_variable_get(:@attempts_made)
-  		game.guess("1234")
-  		expect(game.instance_variable_get(:@attempts_made)).to eql(old + 1)
-  	  end
+      it "increments attempts that are made" do
+        old = game.instance_variable_get(:@attempts_made)
+        game.guess("1234")
+        expect(game.instance_variable_get(:@attempts_made)).to eql(old + 1)
+      end
 
-  	  it "makrs up user's guess"
-  	end 
+      it "makrs up user's guess"
+    end 
 
-  	context "#hint" do
-  	  let(:game) {Game.new}
+    context "#hint" do
+      let(:game) {Game.new}
 
-  	  before {game.start}
+      before {game.start}
 
-  	  it "increments hints that are made" do
-  	    old = game.instance_variable_get(:@hints_made)
-  	    game.hint
-  	    expect(game.instance_variable_get(:@hints_made)).to eql(old + 1)
-  	  end
-  	end
+      it "increments hints that are made" do
+        old = game.instance_variable_get(:@hints_made)
+        game.hint
+        expect(game.instance_variable_get(:@hints_made)).to eql(old + 1)
+      end
+    end
   end
 end
