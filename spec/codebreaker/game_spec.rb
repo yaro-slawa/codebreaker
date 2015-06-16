4129f3f3668a5b091f213b7091970908e0386ea0
Yaroslawa Chernopischenko <chernopyshenko@gmail.com>
2015-06-16 13:23:01 +0300

    Change specs on hint and submit_guess

--- spec/codebreaker/game_spec.rb
+++ spec/codebreaker/game_spec.rb
@@ -10,30 +10,30 @@ module Codebreaker
       it "generates secret code" do
         expect(game.instance_variable_get(:@secret_code)).not_to be_empty 
       end
+
       it "saves 4 numbers secret code" do
         expect(game.instance_variable_get(:@secret_code).length).to eql(4)
       end
+
       it "saves secret code with numbers from 1 to 6" do
         expect(game.instance_variable_get(:@secret_code)).to match(/[1-6]+/)
       end
     end
 
-    context "#guess" do
+    context "#submit_guess" do
       let(:game) {Game.new}
 
       before {game.start}
 
-      it "receive user's guess"
+      it "makrs up user's guess" do
+        marked_guess = game.submit_guess("1234")
+        expect(marked_guess).to match(/(^$ | *[+-])/)
+      end
 
       it "increments attempts that are made" do
         old = game.instance_variable_get(:@attempts_made)
-        game.guess("1234")
+        game.submit_guess("1234")
         expect(game.instance_variable_get(:@attempts_made)).to eql(old + 1)
-      end
-
-      it "makrs up user's guess" do
-        marked_guess = game.guess("1234")
-        expect(marked_guess).to match(/(^$ | *[+-])/)
       end
     end 
 
@@ -42,13 +42,15 @@ module Codebreaker
 
       before {game.start}
 
+      it "shows hint" do
+        expect(game.hint).to match(/[1-6]([*])\1{2}/)
+      end
+
       it "increments hints that are made" do
         old = game.instance_variable_get(:@hints_made)
         game.hint
         expect(game.instance_variable_get(:@hints_made)).to eql(old + 1)
       end
-
-      it "shows hint"
     end
   end
 end
